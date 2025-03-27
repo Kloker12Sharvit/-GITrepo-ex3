e770bdb9cb216d4ace9dd27bf1234f777ddfb780
Kloker12Sharvit <avivsa88@hotmail.com>
2025-03-27 20:46:57 +0200

    ba ba yaga

--- AvivSharvit.java
+++ AvivSharvit.java
@@ -1,7 +1,7 @@
 public class AvivSharvit{
 	public static void main(String[] args){
 		for(int i=1;i<=10;i++){
-			System.out.println("Aviv Sharvit"+i);
+			System.out.println("the baba yaga"+i);
 		}
 	}
 }
